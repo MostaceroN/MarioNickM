2223c2afb0c8688d2e608355185a55e019ffa916
Nicholas Mostacero <nickmostacero@gmail.com>
2014-12-05 11:00:14 -0800

    commit #2

--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -3,7 +3,16 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
-		; // TODO
+		me.game.world.addChild( new me.Sprite (0, 0, me.loader.getImage('title-screen')), 3);
+                me.input.bindKey(me.input.KEY.ENTER, "start");
+                
+                
+                this.handler = me.eventsubscribe(me.event.KEYDOWN, function (action, keyCode, edge){
+                    if(action === "start"){
+                        me.state.change(me.state.PLAY);
+                    }
+                    
+                });
 	},
 	
 	
